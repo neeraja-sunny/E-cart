abb2b916bbbfb1d401b83cfb0487f30c74a688ec
neeraja-sunny <neerajasunny25@gmail.com>
2025-01-14 13:11:02 +0530

    seventh commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,10 +22,24 @@ const Home = () => {
         dispatch(fetchProducts())
     }, [])
 
+     const navigateToNextPage =()=>{
+        if(currentPage!=totalPages){
+            setCurrentPage(currentPage+1)
+        }
+     }
+
+     const navigateToPrevPage =()=>{
+        if(currentPage!=1){
+            setCurrentPage(currentPage-1)
+        }
+     }
+
+
+
     return (
         <>
             <Header insideHome={true} />
-            <div style={{ paddingTop: '100px' }} className='container px-1 mx-auto'>
+            <div style={{ paddingTop: '100px' }} className='container px-1 mx-auto' >
                 {
                     loading ?
                         <div className='flex justify-center items-center my-5 text-lg'>
@@ -37,7 +51,7 @@ const Home = () => {
                             <div className='grid grid-cols-4 gap-4'>
                                 {
                                     allProducts?.length > 0 ?
-                                    allProducts?.map(product=>(
+                                    visibleAllProducts?.map(product=>(
                                         <div key={product?.id} className='rounded border p-2 shadow'>
                                             <img width={'100%'} height={'100%'} src={product?.thumbnail} alt="" />
                                             <div className='text-center'>
@@ -52,6 +66,11 @@ const Home = () => {
                                         </div>
                                 }
 
+                            </div>
+                            <div className='text-2xl text-center font-bold mt-20'>
+                                <span onClick={navigateToPrevPage} className='cursor-pointer'><i className='fa-solid fa-backward me-5'></i></span>
+                                <span>{currentPage} of {totalPages}</span>
+                                <span onClick={navigateToNextPage} className='cursor-pointer'><i className='fa-solid fa-forward ms-5'></i></span>
                             </div>
                         </>
                 }
